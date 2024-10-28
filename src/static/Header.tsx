915113ae60791f8fe19e44ef0bb04a34d0fbeac0
ayodtrader12 <ibrahimpopoola292@gmail.com>
2024-10-28 16:47:46 -0700

    update

---
+++ src/static/Header.tsx
@@ -0,0 +1,35 @@
+// import env from "../assets/pics/enva-removebg-preview.png";
+// const Header = () => {
+//   return (
+//     <div>
+//       <div className="bg-neutral-950 w-full h-[15vh] text-white flex justify-center items-center fixed z-40">
+//         <div className="w-[95%] h-[10vh] flex justify-between items-center">
+//           <div className="flex gap-4">
+//             <div className="w-[15vw] h-[15vh] flex items-center">
+//               <img src={env} alt="djw" className="w-[12vw] h-[12vh]" />
+//             </div>
+//             <div className="flex items-center">
+//               <input type="text" className="w-[50vw]" />
+//             </div>
+//           </div>
+//           <div className="flex gap-4 items-center">
+//             <button className="px-3 py-2 rounded-md bg-lime-400 text-black font-semibold">
+//               Get unlimited downloads
+//             </button>
+//             <div className="font-normal">sign in</div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
+import React from "react";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
